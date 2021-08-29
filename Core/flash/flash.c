9505f0a01cce71ff9a6bd4e98ccabaf2987059ee
KARMA-E <kdi55@ya.ru>
2021-08-29 20:24:42 +0300

    refactoring

--- Core/flash/flash.c
+++ Core/flash/flash.c
@@ -19,13 +19,17 @@ void flash_lock()
 // Готовность flash
 uint8_t flash_ready(void)
 {
-	if(FLASH->SR & FLASH_SR_EOP_Msk)
+#if (0)
+	if(FLASH->SR & FLASH_SR_EOP)
 	{
-		FLASH->SR &=~ FLASH_SR_EOP_Msk;
+		FLASH->SR &=~ FLASH_SR_EOP;
 		return 1;
 	}
 
 	return 0;
+#else
+	return !(FLASH->SR & FLASH_SR_BSY);
+#endif
 }
 
 
@@ -33,17 +37,17 @@ uint8_t flash_ready(void)
 void flash_erase_page(uint32_t addr)
 {
 	// Выбор режима стирания
-	FLASH->CR|= FLASH_CR_PER_Msk;
+	FLASH->CR|= FLASH_CR_PER;
 
 
 	FLASH->AR = addr;
 
 	// Запустить стирание
-	FLASH->CR|= FLASH_CR_STRT_Msk;
+	FLASH->CR|= FLASH_CR_STRT;
 
 	while(!flash_ready());
 
-	FLASH->CR &=~ FLASH_CR_PER_Msk;
+	FLASH->CR &=~ FLASH_CR_PER;
 }
 
 
@@ -68,7 +72,7 @@ void flash_write(uint32_t addr,uint32_t data)
 
 	addr += 2;
 	data >>= 16;
-	*(__IO uint16_t*)addr = (uint16_t) data;
+	*(__IO uint16_t*)addr  = (uint16_t) data;
 
 	while(!flash_ready());
 
